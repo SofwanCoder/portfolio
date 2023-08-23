af517396636815b750cff32a968c71365196f334
Sofwa Lawal <sofwancoder@gmail.com>
2023-08-23 22:38:29 +0100

    chore: discuss page update

--- src/views/DiscussView.vue
+++ src/views/DiscussView.vue
@@ -1,21 +1,38 @@
 <template>
   <div class="about">
-    <h1 class="text-center">Let's Talk</h1>
+    <h1 class="display-6 fw-bold">Let's Talk</h1>
 
-    <div class="fs-5">
-      <p>Hi Techie</p>
+    <div class="fs-6">
+      <p>Greetings!</p>
 
       <p>
-        I like to engage in technical Software Engineering conversation such as
-        Frontend engineering, Backend engineering, Proxies, System design,
-        Networking and Databases. I'm a fan of discussing highly complicated
-        scalable systems and architecture.
+        I have a strong inclination towards engaging in profound conversations
+        centered around the realm of technical Software Engineering. This
+        encompasses a broad spectrum of topics, including but not limited to
+        Frontend Engineering, Backend Engineering, Proxies, System Design,
+        Networking, and Databases.
       </p>
 
       <p>
-        Have something in mind for us to discuss? an Idea? a good conversation?
-        an honest code review? Yeah! why not. Find a free slot on my calender by
-        using the button below.
+        I hold a deep fascination for delving into intricate and highly scalable
+        systems and architectures. If you possess a subject, an idea, or a
+        concept that you believe warrants comprehensive exploration, I invite
+        you to share it with me. Additionally, if you seek an earnest and
+        meticulous system-design/code review, I am more than willing to provide
+        my insights.
+      </p>
+
+      <p>
+        To facilitate our interaction, I offer the convenience of scheduling a
+        discussion at your convenience. Please utilize the button provided below
+        to access my calendar and identify an available slot that aligns with
+        your schedule.
+      </p>
+
+      <p class="fw-bold">
+        I eagerly anticipate the opportunity to engage in enlightening
+        discussions and collaborative exploration of the vast landscape of
+        technical Software Engineering.
       </p>
 
       <div class="text-center">
